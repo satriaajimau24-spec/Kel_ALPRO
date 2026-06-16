62cd2b2722a472ae74e174d0fe7f3ff20a26928c
yumnadhiyaul <yumnadhiyaulauliyah@gmail.com>
2026-06-16 07:34:25 +0700

    (+) insert sort di statistik

--- logical/statistik.go
+++ logical/statistik.go
@@ -1,8 +1,6 @@
 package logical
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func Statistik() {
 	fmt.Println("\n=== STATISTIK ===")
@@ -26,29 +24,28 @@ func Statistik() {
 		}
 	}
 
-	type Item struct {
-		teks  string
-		nilai int
-	}
+	teks := []string{"Saham: %d\n", "Obligasi: %d\n", "ReksaDana: %d\n"}
+	nilai := []int{saham, obligasi, reksadana}
 
-	list := []Item{
-		{"Saham: %d\n", saham},
-		{"Obligasi: %d\n", obligasi},
-		{"ReksaDana: %d\n", reksadana},
-	}
+	// LOGIKA INSERTION SORT
 
-	// Logika Insertion Sort
 	for i := 1; i < 3; i++ {
-		key := list[i]
+		keyNilai := nilai[i]
+		keyTeks := teks[i]
+
 		j := i - 1
-		for j >= 0 && list[j].nilai < key.nilai {
-			list[j+1] = list[j]
+
+		for j >= 0 && nilai[j] > keyNilai {
+			nilai[j+1] = nilai[j]
+			teks[j+1] = teks[j]
 			j--
 		}
-		list[j+1] = key
+
+		nilai[j+1] = keyNilai
+		teks[j+1] = keyTeks
 	}
 
-	for _, item := range list {
-		fmt.Printf(item.teks, item.nilai)
+	for i := 0; i < 3; i++ {
+		fmt.Printf(teks[i], nilai[i])
 	}
 }
